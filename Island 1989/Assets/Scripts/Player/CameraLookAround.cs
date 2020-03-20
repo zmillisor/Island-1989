0b03009b0784c905f91dfb2dc630b1a1670f769c
Epiczd <nutball37@gmail.com>
2020-03-20 09:55:08 -0400

    Player Movement/CameraLookAround

---
+++ Island 1989/Assets/Scripts/Player/CameraLookAround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAround : MonoBehaviour
+{
+    //MouseSensitivity
+    private float mouseSensitivity = 100f;
+    //rotation of the x axis
+    private float xRotation = 0f;
+    //The player
+    [SerializeField] private Transform player;
+
+    //On Start, the mouse cursor is locked
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCamera();
+    }
+
+    void UpdateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Island 1989/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Speed the player moves
+    private float moveSpeed = 20f;
+
+    //Determines if the player is grounded
+    private bool isGrounded = false;
+
+    //Updates Movement and Jumping
+    void Update()
+    {
+        UpdateMovement();
+        Jump();
+
+        Debug.Log(isGrounded);
+    }
+
+    //Updates the player's movement
+    void UpdateMovement()
+    {
+        //WSAD/Arrow keys for movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Lets the player move forward/backward
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * z);
+        //Lets the player move left/right
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * x);
+    }
+
+    //Updates jumping
+    void Jump()
+    {
+        //If the player presses space, and is grounded, they will jump
+        if(Input.GetButton("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 12f, 0f), ForceMode.Impulse);
+        }
+    }
+
+    //If the player is on the ground, isGrounded is true
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    //If the player is NOT on the ground, isGrounded is false
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
